46c520129f630cc9d03b698ea19fca650a5fd293
MFFTD <156596089+MFFTD@users.noreply.github.com>
2024-05-31 16:17:23 +0300

    modify check_if_team_leader function to work with cogs.invite invite_to_team method

--- xlpc/utils/team_utils.py
+++ xlpc/utils/team_utils.py
@@ -3,19 +3,18 @@ from utils.db import AsyncSQLiteDB
 
 db = AsyncSQLiteDB(botconfig.db_path)
 
-async def check_if_team_leader(author):
+async def check_if_team_leader(author) -> Optional[str]:
     try:
         check_if_team_leader_query = "SELECT team_name FROM team WHERE team_leader = ?"
         params = (author,)
         result = await db.execute_query(check_if_team_leader_query, params, fetchall=True)
         
         if not result:
-            return False
+            return None
 
         team_name = result[0][0]
         return team_name
     except Exception as e:
         await interaction.response.send_message("Error inviting to the team")
         print(f"Error inviting user to the team: {e}")
-        return
-        
+        return None
